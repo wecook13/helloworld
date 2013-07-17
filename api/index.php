a695feb4f5c0ed5dfced72d3a04980f877c3a908
wecook13 <wecook13@gmail.com>
2013-07-17 15:03:00 -0700

    new changes

--- api/index.php
+++ api/index.php
@@ -1,5 +1,7 @@
 <?php
 
+define('AWS_PHOTO_BUCKET', 'wecooktests3');
+
 foreach ($_SERVER as $k => $v) {
   echo "$k = $v";
   echo "<br>";
@@ -17,5 +19,12 @@ foreach ($_POST as $k => $v) {
   echo "<br>";
 }
 
+echo "<br>";
+foreach ($_FILES as $k => $v) {
+  if (strstr($v['type'], 'image/')) {
+    echo 'prepare to upload ' . $v['name'];
+    echo "<br>";
+  }
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
-echo "Hello World";
+echo "<html>";
+echo "<body>";
+
+$file_input_name = 'file';
+if (!empty($_FILES[$file_input_name])) {
+  if (!$_FILES[$file_input_name]['error']) {
+    foreach ($_FILES[$file_input_name] as $k => $v) {
+      echo "$k : $v<br>";
+    }
+  }
+}
+
+$upload_url = 'index.php';
+echo "<form action=$upload_url method='post' enctype='multipart/form-data'>";
+echo "<label for='file'>Filename:</label>";
+echo "<input type='file' name=$file_input_name id='file'><br>";
+echo "<input type='submit' name='submit' value='Submit'>";
+echo "</form>";
+
+echo "</body>";
+echo "<html>";
+
 
 ?>
